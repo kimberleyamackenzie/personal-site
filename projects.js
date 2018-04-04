3dadb079ff6c64541109ab76587f561e1acbef59
Kimberley Mackenzie <kimberley.mackenzie@comcast.com>
2018-04-03 17:10:15 -0700

    Additional projects added.

--- projects.js
+++ projects.js
@@ -1,21 +1,86 @@
-    const modal = document.getElementById('the-modal');
-    const openModal = document.getElementById('project');
-    const closeModal = document.getElementsByClassName("close")[0];
-    
-    
-    $(openModal).click(function() {
-        console.log('modal click');
-      modal.style.display = "block";
-    });
+$('#sourceress').click(function () {
+  $('#sourceress-modal').style.display = "block";
+});
+
+closeModal.onclick = function () {
+  $('#sourceress-modal').style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    $('#sourceress-modal').style.display = "none";
+  }
+};
+
+$('#audiomorph').click(function() {
+  $('#audiomorph-modal').style.display = "block";
+});
+
+closeModal.onclick = function() {
+  $('#audiomorph-modal').style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    $('#audiomorph-modal').style.display = "none";
+  }
+};
+
+$('#keyboard').click(function() {
+  $('#keyboard-modal').style.display = "block";
+});
+
+closeModal.onclick = function() {
+  $('#keyboard-modal').style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    $('#keyboard-modal').style.display = "none";
+  }
+};
+
+$('#trek').click(function() {
+  $('#trek-modal').style.display = "block";
+});
+
+closeModal.onclick = function() {
+  $('#trek-modal').style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    $('#trek-modal').style.display = "none";
+  }
+};
+
+$('#muncher').click(function() {
+  $('#muncher-modal').style.display = "block";
+});
+
+closeModal.onclick = function() {
+  $('#muncher-modal').style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    $('#muncher-modal').style.display = "none";
+  }
+};
+
+$('#static').click(function() {
+  $('#static-modal').style.display = "block";
+});
+
+closeModal.onclick = function() {
+  $('#static-modal').style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    $('#static-modal').style.display = "none";
+  }
+};
 
 
-    closeModal.onclick = function() {
-      modal.style.display = "none";
-    };
 
-
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
